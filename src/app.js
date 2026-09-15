9971b7dba9c91e0ac8822f8b67b82d27466c3ae7
Mariana Avirama <mariana.avirama07@gmail.com>
2026-09-15 16:27:15 -0500

    se suben cambios

--- src/app.js
+++ src/app.js
@@ -28,10 +28,4 @@ app.use((req, res) => {
 });
 app.use(manejadorErrores); // SIEMPRE el ultimo
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
-
 export default app;
